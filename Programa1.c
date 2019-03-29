2e08fd2ff540ef61fb9bfe11779e6efe8d2e648a
jmeanf00 <47829733+jmeanf00@users.noreply.github.com>
2019-03-29 12:16:53 +0100

    Add files via upload

---
+++ Programa1.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+   {
+   int n;
+   int *v;
+   int i;
+   int min;
+   printf("Dame un número\n");
+    scanf("%d",&n);
+      v=(int *)malloc(n*sizeof(int));
+    for(i=0;i<n;i++)
+   {
+    scanf("%d",&*(v+i));
+   }
+
+    min =*(v+0);
+    for(i=1;i<n;i++)
+   {
+    if(*(v+i)<min)
+    min=*(v+i);
+   }
+   printf("%d",min);
+    free(v);
+     return 0;
+   }
+
